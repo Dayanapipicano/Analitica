a4fc7e74201b01810e8be38e2fdf170f740c7862
laravelBasic <pipicanodayana2005@gmail.com>
2024-07-09 18:27:44 -0500

    cambios

--- static/bootstrap/js/password_reset_form.js
+++ static/bootstrap/js/password_reset_form.js
@@ -1,33 +1,41 @@
-document.getElementById('password_reset_form').addEventListener('submit', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
     var emailInput = document.getElementById('id_email');
     var errorEmailFormat = document.getElementById('error_email_format');
     var errorEmailNotFound = document.getElementById('error_email_not_found');
 
-    // Validación de formato de correo electrónico antes de enviar el formulario
-    var isValidEmailFormat = /\S+@\S+\.\S+/.test(emailInput.value);
-    if (!isValidEmailFormat) {
-        errorEmailFormat.style.display = 'block';
-        errorEmailNotFound.style.display = 'none';  // Ocultar mensaje de correo no encontrado
-        event.preventDefault();  // Evita enviar el formulario si hay errores
-    } else {
-        errorEmailFormat.style.display = 'none';
-    }
+    emailInput.addEventListener('input', function(event) {
+        var isValidEmailFormat = /\S+@\S+\.\S+/.test(emailInput.value);
+        if (!isValidEmailFormat) {
+            errorEmailFormat.style.display = 'block';
+            errorEmailNotFound.style.display = 'none';
+        } else {
+            errorEmailFormat.style.display = 'none';
+        }
+    });
 
-    // Verificación de existencia del correo electrónico en la base de datos
-    var url = validarEmailURL + '?email=' + emailInput.value;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.exists) {
-                errorEmailNotFound.style.display = 'none';
-                document.getElementById('password_reset_form').submit();  // Envía el formulario si el correo existe
-            } else {
-                errorEmailNotFound.style.display = 'block';
-            }
-        })
-        .catch(error => {
-            console.error('Error al verificar el correo electrónico:', error);
-        });
+    document.getElementById('password_reset_form').addEventListener('submit', function(event) {
+        var isValidEmailFormat = /\S+@\S+\.\S+/.test(emailInput.value);
+        if (!isValidEmailFormat) {
+            errorEmailFormat.style.display = 'block';
+            errorEmailNotFound.style.display = 'none';
+            event.preventDefault();
+        } else {
+            errorEmailFormat.style.display = 'none';
 
-    event.preventDefault();  // Evita enviar el formulario mientras se verifica
+            var url = validarEmailURL + '?email=' + emailInput.value;
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.exists) {
+                        errorEmailNotFound.style.display = 'none';
+                    } else {
+                        errorEmailNotFound.style.display = 'block';
+                        event.preventDefault();
+                    }
+                })
+                .catch(error => {
+                    console.error('Error al verificar el correo electrónico:', error);
+                });
+        }
+    });
 });
