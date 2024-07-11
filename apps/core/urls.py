42d70c48ac8406c49d496f437decbfc78f034071
dayana <pipicanodayana2005@gmail.com>
2024-07-11 11:58:14 -0500

    mapa

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,4 +5,6 @@ from apps.core import views as core_views
 app_name = 'cores'
 urlpatterns = [
     path('crear_metas_formacion', core_views.Crear_metas_formacion, name='Crear_metas_formacion'),
+    path('cobertura_mapa/', core_views.Cobertura_mapa.as_view(), name='cobertura_mapa'),
+    path('cober/', core_views.cober, name='cober'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from apps.personas.models import Persona
+from apps.personas.models import Persona, P04
+from django.http import JsonResponse
+from django.views import View
+
+from django.views.generic import TemplateView
+
 def menu(request):
     return render(request,'home.html')
 
@@ -8,9 +13,6 @@ def inicio_sesion(request):
 
 def registro(request):
     return render(request, 'registro.html')
-
-def cobertura(request):
-    return render(request, 'Cobertura/cobertura.html')
 
 
 def desercion(request):
@@ -50,3 +52,28 @@ def administrador(request):
 
 def Crear_metas_formacion(request):
     return render(request, 'Estrategias_institucionales/crear_meta_formacion.html')
+
+
+
+#COBERTURA
+
+def cobertura(request):
+    return render(request, 'Cobertura/cobertura.html')
+def cober(request):
+    return render(request, 'Cobertura/cober.html')
+
+class Cobertura_mapa(TemplateView):
+    template_name = 'Cobertura/cobertura.html'
+    
+    def get(self, request, *args, **kwargs):
+        nombre_municipio = request.GET.get('nombre_municipio', None)
+        programas_lista = []
+        
+        if nombre_municipio:
+            programas = P04.objects.filter(nombre_municipio_curso=nombre_municipio).values_list('nombre_programa_formacion', flat=True)
+            programas_lista = list(programas)
+        
+        context = self.get_context_data(programas_lista=programas_lista)
+        return self.render_to_response(context)
+
+ 
