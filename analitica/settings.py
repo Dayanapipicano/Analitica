9b18dce568f51eaf03d633fdd12a06280f125ca4
dayana <pipicanodayana2005@gmail.com>
2024-09-18 10:04:48 -0500

    ajuste

--- analitica/settings.py
+++ analitica/settings.py
@@ -84,14 +84,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'vFaZkEvjtbKqkrRJoMUCOdFaQxheWQVM',
-        'HOST':'mysql.railway.internal',
-        'PORT': '27787',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
