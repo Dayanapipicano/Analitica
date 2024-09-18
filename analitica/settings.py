72dfbd84d37330e9cf466b9a0f90b9d58bc939ca
dayana <pipicanodayana2005@gmail.com>
2024-09-18 09:39:44 -0500

    ajuste

--- analitica/settings.py
+++ analitica/settings.py
@@ -95,6 +95,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
